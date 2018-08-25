5245d5e1b049ec47eeef24bfbf6b2c97630a071b
adil1214 <adil1350@gmail.com>
2018-08-25 19:14:57 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const generatePassword = require('password-generator');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/passwords', (req, res) => {
+  const count = 5;
+
+  const passwords = Array.from(Array(count).keys()).map(i =>
+    generatePassword(12, false)
+  );
+
+  res.json(passwords);
+
+  console.log(`Sent ${count} passwords`);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Password generator listening on ${port}`);
+});
+
